3ee8a47ac21d6cfc30e571b4033b2d71a9a8cfa2
Raul Nascimento <raul.hnr23@gmail.com>
2022-08-17 23:01:11 -0300

    Raul Ambiente React criado, imagens adcionadas e organização das pastas iniciada

---
+++ urbicola/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Initial } from './pages/initial/Initial';
+
+function App() {
+  return (
+    <div className="App">
+      <Initial/>
+    </div>
+  );
+}
+
+export default App;
